ac4bd341461c178751f4ff652182b07942fc2b34
BruceWayne175 <75173826+BruceWayne175@users.noreply.github.com>
2020-12-15 14:04:08 +0530

    Update updater.py

--- telebot/plugins/updater.py
+++ telebot/plugins/updater.py
@@ -24,8 +24,8 @@ requirements_path = path.join(
 
 HEROKU_API_KEY = Var.HEROKU_API_KEY
 HEROKU_APP_NAME = Var.HEROKU_APP_NAME
-GIT_REPO_NAME = "TeleBot"
-UPSTREAM_REPO_URL = "https://github.com/xditya/TeleBot"
+GIT_REPO_NAME = "Bruce's Bot"
+UPSTREAM_REPO_URL = "https://github.com/BruceWayne175/TeleBot"
 
 xxxx = CMD_HNDLR if CMD_HNDLR else "."
 
