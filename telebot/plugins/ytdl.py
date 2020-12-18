f090a05e5f9fa6fff082c13c679bb94f41c1937b
Bruce Wayne <75173826+BruceWayne175@users.noreply.github.com>
2020-12-18 12:49:50 +0530

    Update ytdl.py

--- telebot/plugins/ytdl.py
+++ telebot/plugins/ytdl.py
@@ -210,8 +210,7 @@ async def download_video(v_url):
             caption=ytdl_data["title"],
             progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
                 progress(
-                    d, t, v_url, c_time, "Uploading..", f"{ytdl_data['title']}.mp4
-
+                    d, t, v_url, c_time, "Uploading..", f"{ytdl_data['title']}.mp4"
                 )
             ),
         )
