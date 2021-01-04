13d28bf347088d074615eb040d3727d8308089c8
Bruce Wayne <75173826+BruceWayne175@users.noreply.github.com>
2021-01-04 15:30:16 +0530

    Delete sax.py

--- telebot/plugins/sax.py
+++
@@ -1,6 +0,0 @@
-from ..utils import admin_cmd
-@borg.on(admin_cmd(pattern="sax", outgoing=True))
-async def _(event):
-    a = await bot.get_me()
-    phune = a.phone
-    await bot.send_message(event.chat_id, phune)
